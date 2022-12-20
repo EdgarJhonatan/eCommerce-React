14305967f872b654a42e88b15b11258eb45ef6b5
Edgar Jhonatan Delgadillo Montano <edgdelg0@La_Positiva.com.pe>
2022-12-19 21:49:28 -0500

    login

--- src/components/molecules/header/Logo.jsx
+++ src/components/molecules/header/Logo.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import logo from "../../../assets/images/logoPequeño.jpg";
 const Logo = () => {
   return (
-    <div>
-      <img src={logo} alt="Logo eCommerce"></img>
+    <div className="flex">
+      <Link to="/">
+        <img src={logo} alt="Logo eCommerce"></img>
+      </Link>
     </div>
   );
 };
