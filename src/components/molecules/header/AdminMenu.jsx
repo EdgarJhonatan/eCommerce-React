67ba761cc8d0016c4ba4251fe5c261e085ba84b4
Edgar Jhonatan Delgadillo Montano <edgdelg0@La_Positiva.com.pe>
2023-02-22 19:31:22 -0500

    proyecto avanzado

--- src/components/molecules/header/AdminMenu.jsx
+++ src/components/molecules/header/AdminMenu.jsx
@@ -5,8 +5,13 @@ const AdminMenu = () => {
     <nav className="w-full">
       <ul className="flex justify-end text-gray-100">
         <li className="flex items-center">
-          <Link className="menu-item" to="/productos">
+          <Link className="menu-item" to="/admin/productos">
             Productos
+          </Link>
+        </li>
+        <li className="flex items-center">
+          <Link className="menu-item" to="/admin/ventas">
+            Ventas
           </Link>
         </li>
         <li className="flex items-center">
