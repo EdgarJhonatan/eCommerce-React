2d95312afcd6b1e9560393415e42b9098189a987
Edgar Jhonatan Delgadillo Montano <edgdelg0@La_Positiva.com.pe>
2023-02-22 20:09:03 -0500

    modificando Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <Banner />
       <div>
-        <h2 className="mt-8 text-2xl">Últimos productos</h2>
+        <h2 className="mt-8 text-2xl">Últimos productos ingresados</h2>
         <MainProducts />
       </div>
     </>
